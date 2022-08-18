c27dabb917f236a488a364807ca3ab4fc4aa10a4
SlavikusVOG <slavaolish@gmail.com>
2022-08-18 20:35:37 +0300

    feat: add constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,8 @@
 export default class Constants {
   //  static readonly CONSTANT_NAME = VALUE;
+  static readonly JWT_REFRESH_SECRET_KEY = process.env.JWT_REFRESH_SECRET_KEY;
+
+  static readonly JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
+  static readonly MONGO_CONNECTION_STRING = process.env.JWT_SECRET_KEY;
 }
