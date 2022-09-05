4f1e1de1462873fa155bac86e530209919869999
Slava Olishevko <slavaolish@gmail.com>
2022-09-05 11:11:50 +0300

    feat: add graphic function

--- src/view/statistics/statistics-view.ts
+++ src/view/statistics/statistics-view.ts
@@ -1,4 +1,4 @@
-import d3 from 'd3';
+import d3, { range } from 'd3';
 import Constants from '../../constants';
 import HTMLConstructor from '../components/constructor';
 import { services } from '../../services/services';
@@ -34,21 +34,51 @@ export default class Statistics {
     }
   }
 
-  async drawDiagramm() {
+  async drawDiagrams() {
     const statistics = await services.statisticsService.getStatisticsData();
+    const width = 800;
+    const height = 400;
     if (statistics) {
-      const { learnedWords } = statistics;
-      const learnedWordsPerDay: DailyStatistics | undefined = statistics?.optional?.learnedWordsPerDay;
+      // const { learnedWords } = statistics;
+      const learnedWordsPerDay: Array<DailyStatistics> | undefined = statistics?.optional?.learnedWordsPerDay;
       if (learnedWordsPerDay) {
-        const svg = d3.select('#statistics-words-id').append('svg').attr('width', 800).attr('height', 400);
-        const lineFunc = d3
-          .line()
-          .x(function (d) {
-            return d.x;
+        const svg = d3.select('#statistics-words-id').append('svg').attr('width', width).attr('height', height);
+        const x = d3.scaleTyme();
+        DOMImplementation(
+          d3.extent(learnedWordsPerDay, function (d) {
+            return d.date;
           })
-          .y(function (d) {
-            return d.y;
-          });
+        );
+        range([0, width]);
+        svg.append('g').attr('transform', `translate(0, ${height})`).call(d3.axisBottom(x));
+        const y = d3
+          .scaleLinear()
+          .domain([
+            0,
+            d3.max(data, function (d) {
+              return +d.learnedWords;
+            }),
+          ])
+          .range([height, 0]);
+
+        svg.append('g').call(d3.axisLeft(y));
+
+        svg
+          .append('path')
+          .datum(learnedWordsPerDay)
+          .attr('stroke', 'black')
+          .attr('fill', 'none')
+          .attr(
+            'd',
+            d3
+              .line()
+              .x(function (d) {
+                return x(d.date);
+              })
+              .y(function (d) {
+                return y(d.learnedWords);
+              })
+          );
       }
     }
   }
